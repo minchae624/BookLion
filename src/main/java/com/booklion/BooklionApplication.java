62d47e19e8eb7314d23b5752f8bf3b8bace3437f
52kirai52 <52kirai52@gmail.com>
2025-06-25 14:34:04 +0900

    initial commit

---
+++ src/main/java/com/booklion/BooklionApplication.java
@@ -0,0 +1,13 @@
+package com.booklion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BooklionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BooklionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/booklion/BooklionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.booklion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BooklionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
