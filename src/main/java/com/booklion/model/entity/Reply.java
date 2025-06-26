416f653c0d7aa3d81a6309a90952ab02fbfe95c9
minchae624 <minchae0624@gmail.com>
2025-06-26 16:43:38 +0900

    Comment Reply, Category setter 수정(#5)
    * Reply, Category 엔티티
    
    * Reply, Category setter 수정
    
    ---------
    
    Co-authored-by: Sangyun <52kirai52@gmail.com>

--- src/main/java/com/booklion/model/entity/Reply.java
+++ src/main/java/com/booklion/model/entity/Reply.java
@@ -13,6 +13,39 @@ import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+<<<<<<< comment
+
+@Entity
+@Table(name = "Reply")
+@Getter
+@NoArgsConstructor
+public class Reply {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long reply_id;
+	
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "post_id", nullable = false)
+	private Post post;
+	
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "user_id", nullable = false)
+	private Users user;
+	
+	@Column(nullable = false)
+	private String content;
+	
+	private LocalDateTime writingtime;
+
+	public void setContent(String content) {
+		this.content = content;
+	}
+
+	public void setWritingtime(LocalDateTime writingtime) {
+		this.writingtime = writingtime;
+	}
+=======
 import lombok.Setter;
 
 @Entity
@@ -37,4 +70,5 @@ public class Reply {
 	private String content;
 	
 	private LocalDateTime writingtime;
+>>>>>>> dev
 }
