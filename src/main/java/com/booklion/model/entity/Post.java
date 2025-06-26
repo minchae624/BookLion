b2f79f0524ff313c94b3348dab0837c2dd9ec460
HyungJun-An <wns1265@gmail.com>
2025-06-26 13:54:22 +0900

    feature: Post, Like 엔티티 구현

--- src/main/java/com/booklion/model/entity/Post.java
+++ src/main/java/com/booklion/model/entity/Post.java
@@ -1,5 +1,45 @@
 package com.booklion.model.entity;
 
-public class Post {
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name="post")
+@Getter @Setter
+@NoArgsConstructor
+public class Post {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long postId;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private Users user;
+
+    private String title;
+
+    private String content;
+
+    private LocalDateTime writingtime = LocalDateTime.now();
+
+    private String booktitle;
+
+    private String author;
+
+    private String genre;
+
+    private Double rating;
+
+    private int viewCount;
+
+    private int replyCount;
+
+    @OneToMany(mappedBy = "post", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Like> likes = new ArrayList<Like>();
 }
