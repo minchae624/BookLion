499bfe760dc63938ab8cc1d27362197315054d63
minchae624 <minchae0624@gmail.com>
2025-06-26 14:51:30 +0900

    Reply, Category 엔티티 (#2)

--- src/main/java/com/booklion/model/entity/Category.java
+++ src/main/java/com/booklion/model/entity/Category.java
@@ -1,5 +1,26 @@
 package com.booklion.model.entity;
 
-public class Category {
+import java.time.LocalDateTime;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "Category")
+@Getter @Setter
+@NoArgsConstructor
+public class Category {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long category_id;
+	
+	private String category;
 }
