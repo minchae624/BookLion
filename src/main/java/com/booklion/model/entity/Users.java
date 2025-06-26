71d32e2b63887b2520c864dc09260d16602bbdc1
Hyuck Jun Kwon <kwonhj1011@gmail.com>
2025-06-26 13:46:10 +0900

    1st test

--- src/main/java/com/booklion/model/entity/Users.java
+++ src/main/java/com/booklion/model/entity/Users.java
@@ -1,6 +1,38 @@
 package com.booklion.model.entity;
 
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class Users {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer userId;
 
+    @Column(nullable = false, unique = true)
+    private String username;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false)
+    private String email;
+
+    private LocalDateTime createdAt;
+
+    private String comment;
+
+    @PrePersist
+    public void setCreatedAt() {
+        this.createdAt = LocalDateTime.now();
+    }
 }
