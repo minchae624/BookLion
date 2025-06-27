ab9aaa3f50fe585ef7e3cf05ced13da2a185d701
minchae624 <minchae0624@gmail.com>
2025-06-27 17:31:50 +0900

    댓글 기능 구현

---
+++ src/main/java/com/booklion/dto/ReplyRequestDto.java
@@ -0,0 +1,15 @@
+package com.booklion.dto;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+// 댓글 작성/수정 요청용 Dto
+public class ReplyRequestDto {
+	
+    private Long userId;
+    
+    private String content;
+    
+}
