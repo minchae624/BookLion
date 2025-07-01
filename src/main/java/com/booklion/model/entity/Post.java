5b3f9cb1142dd4bbce41064904b4fb0a2985a8e5
HyungJun-An <wns1265@gmail.com>
2025-07-01 16:37:29 +0900

    fix: 페이징 오류 수정 및 게시글 조회 시 줄바꿈 수정

--- src/main/java/com/booklion/model/entity/Post.java
+++ src/main/java/com/booklion/model/entity/Post.java
@@ -3,7 +3,6 @@ package com.booklion.model.entity;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import lombok.Setter;
 
 import java.time.LocalDateTime;
 import java.util.ArrayList;
