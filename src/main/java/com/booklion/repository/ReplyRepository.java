9dcec95f0a3e78b20ceec36f713e21f0953e9e76
minchae624 <minchae0624@gmail.com>
2025-07-02 17:01:50 +0900

    회원탈퇴 - 댓글 삭제

--- src/main/java/com/booklion/repository/ReplyRepository.java
+++ src/main/java/com/booklion/repository/ReplyRepository.java
@@ -3,10 +3,10 @@ package com.booklion.repository;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.booklion.model.entity.Reply;
+import com.booklion.model.entity.Users;
 import com.booklion.model.entity.Post;
 
 @Repository
@@ -17,4 +17,6 @@ public interface ReplyRepository extends JpaRepository<Reply, Long> {
     int countByPost(Post post);
 
     void deleteByPost(Post post);
+    
+    void deleteByUser(Users user);
 }
